0a636c2519ddd2ddec8db602f803ed047e101baa
XuQingLi <2542398121@qq.com>
2025-11-16 13:56:17 +0000

    EVPT

--- Bert.py
+++ Bert.py
@@ -3,7 +3,6 @@ from transformers import BertTokenizer, BertModel
 
 # 指定缓存路径
 os.environ['TRANSFORMERS_CACHE'] = '/home/sun/Endometrial/ICMIL/utils'
-
 model_name = "bert-base-uncased"
 
 # 从自定义路径下载预训练的tokenizer和模型权重
